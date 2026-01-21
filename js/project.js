058df042303dca9c760e77a50ec1fc8968a8f9cd
sanghyek <jsbxiw819224@gmail.com>
2026-01-21 22:38:56 +0900

    update 20260121 project

--- js/project.js
+++ js/project.js
@@ -4,39 +4,12 @@ $(function () {
     var getParam = params.get('name');
 
     switch (getParam) {
-        case 'gallery':
-            $('.portfolio-title').html('갤러리');
-            $('.gallery').removeClass('d-none');
-            break;
-            
-        case 'digitalnow':
-            $('.portfolio-title').html('디지털나우');
-            $('.digitalnow').removeClass('d-none');
+        case 'genie':
+            $('.genie').removeClass('d-none');
             break;
 
-        case 'butlerlounge':
-            $('.portfolio-title').html('버틀러라운지');
-            $('.butlerlounge').removeClass('d-none');
-            break;
-
-        case 'popupsystem':
-            $('.portfolio-title').html('팝업 시스템');
-            $('.popupsystem').removeClass('d-none');
-            break;
-
-        case 'lotto':
-            $('.portfolio-title').html('로또 앱');
-            $('.lotto').removeClass('d-none');
-            break;
-
-        case 'polaroid':
-            $('.portfolio-title').html('폴라로이드 앱');
-            $('.polaroid').removeClass('d-none');
-            break;
-
-        case 'IyouMarket':
-            $('.portfolio-title').html('IyouMarket');
-            $('.IyouMarket').removeClass('d-none');
+        case 'reservation':
+            $('.reservation').removeClass('d-none');
             break;
     }
 });
